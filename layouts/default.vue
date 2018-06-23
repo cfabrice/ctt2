4a7fc19f429adafac175d00014d00ba754e80874
Fabrice Cortat <fab@wonderweb.ch>
2018-06-23 15:47:34 +0200

    mat icon

--- layouts/default.vue
+++ layouts/default.vue
@@ -56,7 +56,7 @@
                 <a class="bd-tw-button button" data-social-network="Twitter" data-social-action="tweet" data-social-target="http://localhost:4000"
                   target="_blank" href="https://twitter.com/intent/tweet?text=Bulma: a modern CSS framework based on Flexbox&amp;hashtags=bulmaio&amp;url=http://localhost:4000&amp;via=jgthms">
                   <span class="icon">
-                    <i class="fab fa-twitter"></i>
+                    <b-icon icon="twitter" />
                   </span>
                   <span>
                     Tweet
@@ -66,7 +66,7 @@
               <p class="control">
                 <a class="button is-primary" href="https://github.com/jgthms/bulma/releases/download/0.7.1/bulma-0.7.1.zip">
                   <span class="icon">
-                    <i class="fas fa-download"></i>
+                    <b-icon icon="download" />
                   </span>
                   <span>Download</span>
                 </a>
@@ -104,7 +104,7 @@
   export default {
     data() {
       return {
-         showNav: false,
+        showNav: false,
         items: [{
             title: 'Home',
             icon: 'home',
