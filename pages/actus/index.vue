2b6141df8649722e1e4307b8f9ff631982bd2196
Fabrice Cortat <fab@wonderweb.ch>
2018-06-24 20:55:28 +0200

    ACtus loop

--- pages/actus/index.vue
+++ pages/actus/index.vue
@@ -1,15 +1,38 @@
 <template>
   <section class="section">
     <div class="columns is-mobile">
-      <h2>Blog</h2>
-      <ul>
-        <li v-for="post in posts" :key="post.date">
-          <nuxt-link :to="post._path">
-            {{ post.title }}
-          </nuxt-link>
-        </li>
+        <div class="column">
+            <h1 class="title">Actualités</h1>
+        </div>
+    </div>
+    <div class="columns is-multiline is-mobile">
+        <div v-for="post in posts" :key="post.title" class="column is-one-third ">
+          <div class="card">
+            <header class="card-header">
+              <p class="card-header-title has-text-grey">
+               <nuxt-link :to="post._path"> {{ post.title }} </nuxt-link>              </p>
+            </header>
+            <div class="card-content">
+              <div class="content has-text-centered">
+                <b-icon :icon="calendar" size="is-large" type="is-primary" />     {{ post.date }}
 
-      </ul>
+                <div v-html="$md.render(post.body)"></div>
+              </div>
+
+            </div>
+
+          </div>
+        </div>
+
+        <!-- <div class="column">
+            <ul>
+                <li v-for="post in posts" :key="post.date">
+                <nuxt-link :to="post._path">
+                    {{ post.title }}
+                </nuxt-link>
+                </li>
+            </ul>
+        </div> -->
     </div>
   </section>
 </template>
