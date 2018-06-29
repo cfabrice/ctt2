ed1863e4cc65de351106405e58ffd58b51de40f9
Fabrice Cortat <fab@wonderweb.ch>
2018-06-29 23:44:40 +0200

    image start

--- pages/actus/index.vue
+++ pages/actus/index.vue
@@ -9,7 +9,7 @@
           <div class="card">
             <div class="card-image">
               <figure class="image ">
-                <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+                <img v-if="post.image" src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
               </figure>
             </div>
             <div class="card-content has-text-centered">
