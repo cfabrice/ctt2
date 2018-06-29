7b9bc8e0b712842ba8a6bcc79207bcef7f87ca7f
Fabrice Cortat <fab@wonderweb.ch>
2018-06-29 23:30:26 +0200

    better footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,13 +1,30 @@
 <template>
   <footer class="footer">
-    <div class="content has-text-centered">
-      <p> ©2018 ·
-        <strong>CTT Puplinge</strong> · Pour tout contact
-        <a href="mailto:info@ctt-puplinge.ch">info@ctt-puplinge.ch</a> · Réalisation:
-        <a href="https://wonderweb.ch">Wonderweb</a>.
-      </p>
+    <div class="level">
+      <div class="level-left">
+        <div class="level-item ">
+          <p> ©2018 ·
+           CTT Puplinge
+          </p>
+        </div>
+      </div>
+       <div class="level-center">
+      <div class="level-item has-text-centered">
+          <p>Pour tout contact
+            <a href="mailto:info@ctt-puplinge.ch">info@ctt-puplinge.ch</a>
+          </p>
+        </div>
+        </div>
+      <div class="level-right">
+        <div class="level-item ">
+          <p> Réalisation:
+            <a href="https://wonderweb.ch">Wonderweb</a>
+          </p>
+        </div>
+      </div>
     </div>
   </footer>
+
 </template>
 <style>
   .footer {
