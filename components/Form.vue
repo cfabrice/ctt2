96f33882282bc51f0eb4c243871235bb04a9925d
Fabrice Cortat <fab@wonderweb.ch>
2018-06-30 08:42:48 +0200

    new form

---
+++ components/Form.vue
@@ -0,0 +1,94 @@
+<template>
+
+<section class="section">
+  <form name="contact" action="/merci" netlify-honeypot="bot-field" method="post" netlify>
+    <input type="hidden" name="form-name" value="contact" />
+    <p class="hidden">
+      <label>Don’t fill this out:
+        <input name="bot-field">
+      </label>
+    </p>
+    <div class="field is-horizontal">
+      <div class="field-body">
+        <div class="field">
+          <p class="control is-expanded has-icons-left">
+            <input class="input" type="text" placeholder="Nom">
+            <span class="icon is-small is-left">
+              <b-icon icon="account" />
+            </span>
+          </p>
+        </div>
+        <div class="field">
+          <p class="control is-expanded">
+            <input class="input" type="text" placeholder="Prénom">
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="field">
+      <p class="control  has-icons-left ">
+        <input class="input" type="text" placeholder="Email">
+        <span class="icon is-small is-left">
+          <b-icon icon="email" />
+        </span>
+        <!-- <span class="icon is-small is-right">
+              <b-icon icon="check" />
+            </span> -->
+      </p>
+    </div>
+    <div class="field">
+
+      <div class="field ">
+        <p class="control has-icons-left">
+          <input class="input" type="tel" placeholder="Votre téléphone">
+          <span class="icon is-small is-left">
+          <b-icon icon="phone" />
+        </span>
+          </p>
+      </div>
+    </div>
+
+
+
+    <div class="field ">
+      <div class="field-body">
+        <div class="field">
+          <div class="control">
+            <input class="input" type="text" placeholder="Votre sujet">
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="field">
+      <div class="field-body">
+        <div class="field">
+          <div class="control">
+            <textarea class="textarea" placeholder="Tapez votre texte ici..."></textarea>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="field ">
+      <div class="field-label">
+        <!-- Left empty for spacing -->
+      </div>
+      <div class="field-body">
+        <div class="field">
+          <div class="control">
+            <button class="button is-primary">
+              Envoyer
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </form>
+  </section>
+</template>
+<style>
+
+
+</style>
--- pages/contact.vue
+++ pages/contact.vue
@@ -1,15 +1,18 @@
 <template>
   <article class="column is-8-tablet">
     <!-- <img v-bind:src="thumbnail" v-bind:alt="title"/> -->
-   <h1 class="title is-1 has-text-centered">{{ title }}</h1>
+    <h1 class="title is-1 has-text-centered">{{ title }}</h1>
     <div class="content">
-         <!-- <p>{{ body }}</p> -->
-     <div v-html="$md.render(body)"></div>
+      <!-- <p>{{ body }}</p> -->
+      <div v-html="$md.render(body)"></div>
+      <Form/>
 
-      <form name="contact" action="/merci" netlify-honeypot="bot-field" method="post" netlify>
+      <!-- <form name="contact" action="/merci" netlify-honeypot="bot-field" method="post" netlify>
         <input type="hidden" name="form-name" value="contact" />
         <p class="hidden">
-          <label>Don’t fill this out: <input name="bot-field"></label>
+          <label>Don’t fill this out:
+            <input name="bot-field">
+          </label>
         </p>
         <label class="form-label" for="name">
           Name:
@@ -24,39 +27,52 @@
         </label>
         <textarea class="form-field" name="message" id="message"></textarea>
         <input class="form-button" type="submit" value="Send message" />
-      </form>
+      </form> -->
     </div>
   </article>
 </template>
 
 <script>
-export
-default {
-    transition: 'page',
-  async asyncData({ params }) {
-    // const postPromise = process.BROWSER_BUILD
-    //   ? import('~/content/blog/posts/' + params.slug + '.json')
-    //   : Promise.resolve(
-    //       require('~/content/blog/posts/' + params.slug + '.json')
-    //     );
+  import Form from '@/components/Form'
 
-   let post = await import('~/content/contact.json');
-    return post;
-  }
-};
+  export
+  default {
+    transition: 'page',
+    components: {
+      Form,
+    },
+    async asyncData({
+      params
+    }) {
+      // const postPromise = process.BROWSER_BUILD
+      //   ? import('~/content/blog/posts/' + params.slug + '.json')
+      //   : Promise.resolve(
+      //       require('~/content/blog/posts/' + params.slug + '.json')
+      //     );
+
+      let post = await
+      import ('~/content/contact.json');
+      return post;
+    }
+  };
+
 </script>
 <style media="screen">
   img {
     max-width: 300px;
   }
+
   .hidden {
     display: none;
   }
+
   form {
     display: flex;
     flex-direction: column;
   }
-[type="submit"] {
-  margin-top:3rem;
-}
+
+  [type="submit"] {
+    margin-top: 3rem;
+  }
+
 </style>
