d42d2b54cbd97b460ed1dc45fb04b72eb9cdd011
Fabrice Cortat <fab@wonderweb.ch>
2018-07-02 09:45:54 +0200

    dynamic routes

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,9 @@ var path = require('path');
 // automatically and match it to the path of your Nuxt routes.
 // The Nuxt routes are generate by Nuxt automatically based on the pages folder.
 var dynamicRoutes = getDynamicPaths({
-  '/blog': 'blog/*.json'
+  '/actus': 'actus/*.json',
+  '': '*.json',
+  '/s': 's/*.json'
 });
 
 module.exports = {
