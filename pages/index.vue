0a0251ea3d5b2b4826baebc5419de1980cbc5341
Fabrice <fabrice@banquise.ch>
2024-01-28 11:11:00 +0100

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -6,8 +6,9 @@
           <!-- <h3 class="title is-3 has-text-centered">Un petit titre</h3> -->
           <p class="has-text-centered accroche">
             Créé en 1978, le CTT Puplinge compte une trentaine de membres, enfants, jeunes et moins jeunes.<br>
-            Plusieurs équipes participent aux  compétitions cantonales et nationales.<br>
-            Différents cours sont dispensés dans les locaux mis à disposition par la commune: salle communale, salle de gymnastique (et abri PC - entraînement libre, robot).</p>
+            Plusieurs équipes participent aux compétitions cantonales et nationales.<br>
+            Différents cours sont dispensés dans les locaux mis à disposition par la commune: salle communale, salle de
+            gymnastique (et abri PC - entraînement libre, robot).</p>
         </div>
       </div>
     </section>
@@ -15,21 +16,13 @@
       <div class="columns is-multiline is-centered">
         <div class="column is-10-tablet">
           <h3 class="title is-3 has-text-centered">Dernières nouvelles</h3>
-          <LastNews/>
+          <LastNews />
         </div>
       </div>
-       <div class="">
-          <div id="nuliga-content-teaser-meetings"></div>
-          <div id="nuliga-content-teaser-ranking"></div>
-          <script
-            src="https://clicktt.sportred.de/v1/lib/ZjA2ZjI3MjdlNzlkMTA5N2M0NzBmMmZjOTY2MTA4NWEwNmM3ZWVlNzQyZGNjOTBlZTg2ZWZlZmU2NDM5NDZjMQDcWtRJ9cVOkwHaGNNe1uVHiY6nGiJKE8MwALVtyIuo.js">
-          </script>
-          <script>
-            hb_api.teaser_groupmeetings({element: document.getElementById('nuliga-content-teaser-meetings'), id:212644});
-            hb_api.teaser_groupranking({element: document.getElementById('nuliga-content-teaser-ranking'), id:212644, emptyLayout:true});
-          </script>
-        </div>
-
+      <div class="">
+        <div id="nuliga-content-teaser-meetings"></div>
+        <div id="nuliga-content-teaser-ranking"></div>
+      </div>
     </section>
     <!-- <section class="section">
       <div class="columns is-mobile">
@@ -56,31 +49,42 @@
 </template>
 
 <script>
-  //import BLogo from '@/components/Logo'
-  import LastNews from '@/components/LastNews'
+//import BLogo from '@/components/Logo'
+import LastNews from '@/components/LastNews'
 
-  export default {
-    transition: 'page',
+export default {
+  transition: 'page',
 
-    layout: 'hero',
-    name: 'HomePage',
-    components: {
-      //BLogo
-      LastNews
-    },
-    data() {
-      return {
+  layout: 'hero',
+  name: 'HomePage',
+  components: {
+    //BLogo
+    LastNews
+  },
+  data() {
+    return {
 
-      }
-    },
-    head () {
-      return {
-        title: 'Accueil',
-        meta: [
-          { hid: 'description', name: 'description', content: 'Bienvenue sur le site du CTT Puplinge' }
-        ]
-      }
+    }
+  },
+  head() {
+    return {
+      title: 'Accueil',
+      meta: [
+        { hid: 'description', name: 'description', content: 'Bienvenue sur le site du CTT Puplinge' }
+      ],
+      script: [
+        { src: 'https://clicktt.sportred.de/v1/lib/ZjA2ZjI3MjdlNzlkMTA5N2M0NzBmMmZjOTY2MTA4NWEwNmM3ZWVlNzQyZGNjOTBlZTg2ZWZlZmU2NDM5NDZjMQDcWtRJ9cVOkwHaGNNe1uVHiY6nGiJKE8MwALVtyIuo.js', async: true },
+      ]
+
+    }
+  },
+  mounted() {
+    if (typeof hb_api !== 'undefined') {
+      hb_api.teaser_groupmeetings({ element: document.getElementById('nuliga-content-teaser-meetings'), id: 212644 });
+      hb_api.teaser_groupranking({ element: document.getElementById('nuliga-content-teaser-ranking'), id: 212644, emptyLayout: true });
     }
   }
+
+}
 
 </script>
